d9017f86c6d97a22aeecf4a3ccff1d0dfb55cf9e
weimeilin79 <weimeilin@gmail.com>
2025-01-03 16:47:46 +0000

    update with code

---
+++ app.py
@@ -0,0 +1,80 @@
+"""
+A sample Hello World server.
+"""
+import os
+import google.generativeai as genai
+from flask import Flask, render_template, request
+from google.cloud import aiplatform
+import vertexai
+from vertexai.generative_models import GenerativeModel
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    message = "It's running!"
+
+    """Get Cloud Run environment variables."""
+    service = os.environ.get('K_SERVICE', 'Unknown service')
+    revision = os.environ.get('K_REVISION', 'Unknown revision')
+
+    return render_template('index.html',
+        message=message,
+        Service=service,
+        Revision=revision)
+
+#function that will route to test.html
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+#function that will route to test.html
+@app.route('/test-vertex')
+def testVertex():
+    return render_template('test-vertex.html')
+
+#function that handles the request(POST) that receives the prompt from test.html and will call the Gemma model in vertex   
+@app.route('/call-gemma', methods=['POST'])
+def handle_prompt():
+    data = request.get_json()
+    prompt = data.get('prompt')
+    token = data.get('token')
+    #Call Gemma model in Vertex AI here.  
+    response = call_gemma_model(prompt,token)
+
+    return response
+
+
+@app.route('/call-vertex', methods=['POST'])
+def handle_prompt_vertex():
+    data = request.get_json()
+    prompt = data.get('prompt')
+
+    #Call Gemma model in Vertex AI here.  
+    response = call_vertex(prompt)
+
+    return response
+#Implement the call_gemma_model using vertex sdk
+
+def call_gemma_model(prompt,token):
+    genai.configure(api_key=token)
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(prompt)
+    return response.text
+
+def call_vertex(prompt):
+    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")  # Get project ID from env
+    if not project_id:
+        raise RuntimeError("GOOGLE_CLOUD_PROJECT environment variable not set.")
+    vertexai.init(project=project_id,)
+    model = GenerativeModel("gemini-pro")
+    response = model.generate_content(prompt)
+    return response.text
+
+
+
+if __name__ == '__main__':
+    server_port = os.environ.get('PORT', '8080')
+    app.run(debug=False, port=server_port, host='0.0.0.0')
+
