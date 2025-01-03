4e434ce8d42a6f8700f191519301774ccc81ef6f
weimeilin79 <weimeilin@gmail.com>
2025-01-03 18:11:15 +0000

    Add chat framework

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-"""
-A sample Hello World server.
-"""
 import os
 import google.generativeai as genai
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from google.cloud import aiplatform
 import vertexai
 from vertexai.generative_models import GenerativeModel
+from flight import *
+
 
 app = Flask(__name__)
 
@@ -34,6 +33,11 @@ def test():
 def testVertex():
     return render_template('test-vertex.html')
 
+#function that will route to test.html
+@app.route('/flight')
+def testFlight():
+    return render_template('flight.html')
+
 #function that handles the request(POST) that receives the prompt from test.html and will call the Gemma model in vertex   
 @app.route('/call-gemma', methods=['POST'])
 def handle_prompt():
@@ -45,7 +49,6 @@ def handle_prompt():
 
     return response
 
-
 @app.route('/call-vertex', methods=['POST'])
 def handle_prompt_vertex():
     data = request.get_json()
@@ -53,8 +56,16 @@ def handle_prompt_vertex():
 
     #Call Gemma model in Vertex AI here.  
     response = call_vertex(prompt)
-
     return response
+
+@app.route('/chatbot', methods=['POST'])
+def handle_chatbot():
+    data = request.get_json()
+    message = data.get('message')
+    response = {"response": message} # Changed key to 'response'
+    print(f"Sending response: {response}") # Debug print statement
+    return jsonify(response)
+    
 #Implement the call_gemma_model using vertex sdk
 
 def call_gemma_model(prompt,token):
